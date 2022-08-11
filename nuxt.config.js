6b1c65052294403c544591b56f79d919711c2694
Matt Coleman <matt.coleman@protonmail.com>
2022-08-11 19:56:48 +0100

    chore: added a testimonail api call

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,7 +2,7 @@ export default {
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
   router: {
-    base: '/arcdisco-frontend/',
+    base: process.env.NODE_ENV === 'production' ? '/arcdisco-frontend/' : '/',
   },
 
   // Global page headers: https://go.nuxtjs.dev/config-head
@@ -45,7 +45,7 @@ export default {
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    baseURL: '/',
+    baseURL: process.env.API_ROUTE,
   },
 
   // PWA module configuration: https://go.nuxtjs.dev/pwa
--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,23 @@
 <template>
-  <Tutorial />
+  <div>
+    <Tutorial />
+    <div v-for="testimonial in testimonials.data" :key="testimonial.id">
+      <p>{{ testimonial.attributes.Author }}</p>
+      <p>{{ testimonial.attributes.Testimonial }}</p>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
   name: 'IndexPage',
+  data() {
+    return {
+      testimonials: [],
+    }
+  },
+  async mounted() {
+    this.testimonials = await this.$axios.$get('/testimonials')
+  },
 }
 </script>
