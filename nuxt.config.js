e38fa4a550b80f1a6f75a37009e94d9c256be281
Matt Coleman <matt.coleman@protonmail.com>
2022-08-11 20:43:38 +0100

    chore: updated env var

--- nuxt.config.js
+++ nuxt.config.js
@@ -51,7 +51,7 @@ export default {
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    baseURL: process.env.API_ROUTE,
+    baseURL: this.$env.API_ROUTE,
   },
 
   // PWA module configuration: https://go.nuxtjs.dev/pwa
