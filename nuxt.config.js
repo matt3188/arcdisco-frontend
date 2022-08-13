5fd15449cfac168add0693d576060a4072772aa8
Matt Coleman <matt.coleman@protonmail.com>
2022-08-13 18:35:50 +0100

    chore: added vue-agile

--- nuxt.config.js
+++ nuxt.config.js
@@ -18,10 +18,10 @@ export default {
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: ['@/assets/css/main.css'],
+  css: ['@/assets/css/main.css', 'vue-agile/dist/VueAgile.css'],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [],
+  plugins: [{ src: './plugins/vue-agile.js', mode: 'client' }],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
