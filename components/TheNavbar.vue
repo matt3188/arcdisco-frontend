3415849dcd212150b4abe7b33af1c6ae2ec0e35e
Matt Coleman <matt.coleman@protonmail.com>
2022-08-16 12:48:01 +0100

    chore: moved main nav to the right

--- components/TheNavbar.vue
+++ components/TheNavbar.vue
@@ -1,16 +1,21 @@
 <template>
   <nav class="p-8">
-    <div class="flex content-center">
+    <div class="flex content-center justify-between">
       <img
         class="max-h-12 mr-5"
         src="https://res.cloudinary.com/dicg2gmh9/image/upload/v1660577899/thumbnail_logo_1134eb0afd.png?width=219&height=192"
       />
-
-      <template v-for="navItem in renderNavigation">
-        <NuxtLink :key="navItem.id" :to="navItem.path" class="self-center mr-3">
-          {{ navItem.title }}
-        </NuxtLink>
-      </template>
+      <div class="flex content-center">
+        <template v-for="navItem in renderNavigation">
+          <NuxtLink
+            :key="navItem.id"
+            :to="navItem.path"
+            class="self-center mr-3"
+          >
+            {{ navItem.title }}
+          </NuxtLink>
+        </template>
+      </div>
     </div>
   </nav>
 </template>
