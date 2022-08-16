c5f1c9ab5ccacfee317f368f48acace067e6a451
Matt Coleman <matt.coleman@protonmail.com>
2022-08-16 22:29:28 +0100

    fix: mobile nav item click area

--- components/TheMainNav.vue
+++ components/TheMainNav.vue
@@ -9,7 +9,7 @@
         <NuxtLink
           :key="navItem.id"
           :to="navItem.path"
-          class="self-start w-full md:w-auto py-3 md:py-0 md:self-center md:mr-3 hover:text-zinc-400 transition-colors no-underline"
+          class="flex self-start w-full md:w-fit py-3 md:py-0 md:self-center md:mr-3 hover:text-zinc-400 transition-colors no-underline"
         >
           {{ navItem.title }}
         </NuxtLink>
