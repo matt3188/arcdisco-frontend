dca6ebe3ff91483a4ac743b0562f0fea2f5bbee3
Matt Coleman <matt.coleman@protonmail.com>
2022-08-16 21:35:24 +0100

    feat: added page transition

--- layouts/default.vue
+++ layouts/default.vue
@@ -5,3 +5,25 @@
     <TheFooter />
   </div>
 </template>
+
+<style>
+.page-enter-active,
+.page-leave-active {
+  transition: opacity 0.5s;
+}
+
+.page-enter,
+.page-leave-to {
+  opacity: 0;
+}
+
+.layout-enter-active,
+.layout-leave-active {
+  transition: opacity 0.5s;
+}
+
+.layout-enter,
+.layout-leave-to {
+  opacity: 0;
+}
+</style>
