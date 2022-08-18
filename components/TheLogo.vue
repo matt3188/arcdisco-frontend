e92d8843b08526ff6477806555ce80e0dc0e4e9b
Matt Coleman <matt.coleman@protonmail.com>
2022-08-18 21:44:01 +0100

    chore: added link to logo

--- components/TheLogo.vue
+++ components/TheLogo.vue
@@ -1,8 +1,10 @@
 <template>
-  <img
-    class="mr-5 max-h-12"
-    src="https://res.cloudinary.com/dicg2gmh9/image/upload/v1660577899/thumbnail_logo_1134eb0afd.png?width=219&height=192"
-  />
+  <NuxtLink to="/">
+    <img
+      class="mr-5 max-h-12"
+      src="https://res.cloudinary.com/dicg2gmh9/image/upload/v1660577899/thumbnail_logo_1134eb0afd.png?width=219&height=192"
+    />
+  </NuxtLink>
 </template>
 
 <script>
